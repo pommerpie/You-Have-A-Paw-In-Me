71ebd8c93a874effd53cd407864b0c28dfb4cca7
Ruminate <inramiland@gmail.com>
2024-02-07 21:53:26 -0500

    commiting before changing branches

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,6 +1,6 @@
 // src/components/Header.js
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Link } from 'react-router-dom';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,11 +13,11 @@ const Header = () => {
     <header>
       <div className="logo">Therapy Animal Support Fund</div>
       <nav className={menuOpen ? 'open' : ''}>
-        <Link to="/">Home</Link>
+        {/* <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
         <Link to="/signup">Signup</Link>
         <Link to="/search">Search</Link>
-        <Link to="/donate">Donate</Link>
+        <Link to="/donate">Donate</Link> */}
       </nav>
       <div className="menu-toggle" onClick={toggleMenu}>
         ☰
