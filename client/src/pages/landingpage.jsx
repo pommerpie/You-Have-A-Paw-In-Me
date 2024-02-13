e8d03277973a7ef62bf5bcad65dc9ae32387cf16
axl <pommerpie@gmail.com>
2024-02-12 21:13:18 -0500

    Styling

--- client/src/pages/landingpage.jsx
+++ client/src/pages/landingpage.jsx
@@ -1,14 +1,25 @@
 // src/pages/LandingPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../components/styles/landing.css';
 
 
 const LandingPage = () => {
   return (
     <div>
       <h1>Welcome to Therapy Animal Support Fund</h1>
-      {/* <Link to="/login">Login</Link>
-      <Link to="/signup">Create Account</Link> */}
+      <p className='LandAbout'>Welcome to the Therapy Animal Support Fund, where compassion meets crowdfunding
+         to create a world where the healing power of therapy animals is accessible to all.
+         Our mission is simple yet profound: to make a positive impact on the lives of individuals
+         facing physical, emotional, or mental challenges by providing them with the comfort and
+         support of therapy animals. Here, you have the opportunity to be a catalyst for change
+         by contributing to our carefully curated selection of nonprofit organizations dedicated
+         to bringing therapy animals to those in need. </p>
+
+         <br>
+         </br>
+       <p> Ready to log in? <Link to="/login">Log in here</Link> </p>
+      <p> Don't have an account? <Link to="/signup">Sign up here</Link></p>
     </div>
   );
 };
