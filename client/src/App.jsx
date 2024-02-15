3500656d763a74d1fd36148bc48767b830e55bbb
Ruminate <inramiland@gmail.com>
2024-02-15 11:03:53 -0500

    updates to app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
 // src/App.js
+import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+//import { Outlet } from 'react-router-dom';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
 import Hero from './components/hero';
@@ -9,17 +18,35 @@ import Filter from './components/filter';
 import Donation from './components/donation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3005/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-      <>
+      <ApolloProvider client={client}>
         <Header />
         <Hero />
         <AboutUs />
         <Filter />
         <Donation />
         <Footer />
-      </>
+      </ApolloProvider>
   );
 }
 
