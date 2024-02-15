5cdeac7f231ba4b7b2f47e101ccc407f83a4034a
Jennifer Winter <jennwintr@gmail.com>
2024-02-14 21:16:55 -0500

    debug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    
       <>
         <Header />
         <Hero />
@@ -21,7 +20,6 @@ function App() {
         <Donation />
         <Footer />
       </>
-    
   );
 }
 
